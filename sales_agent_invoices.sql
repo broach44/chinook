c7b900598bb73334bc646c9c322ed35f7772a58c
Crystal Broach <crysland@gmail.com>
2020-04-25 14:00:06 -0500

    sales agent invoice query complete

---
+++ sales_agent_invoices.sql
@@ -0,0 +1,9 @@
+select 
+	Invoice.InvoiceId, 
+	Customer.FirstName + ' ' + Customer.LastName as CustomerFullName, 
+	Employee.FirstName + ' ' + Employee.LastName as SalesAgentFullName
+from Invoice
+	join Customer
+		on Customer.CustomerId = Invoice.CustomerId
+	join Employee
+		on Customer.SupportRepId = Employee.EmployeeId
