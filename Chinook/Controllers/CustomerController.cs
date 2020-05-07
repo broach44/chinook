34d932266dd1a131cd3d129d667bf6062f56ccb7
Crystal Broach <crysland@gmail.com>
2020-05-07 18:54:10 -0500

    setup started

---
+++ Chinook/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chinook.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chinook.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerRepo _repository = new CustomerRepo();
+
+        // GET: api/Customer
+        [HttpGet("{country}")]
+        public IEnumerable<string> GetByCounty(string country)
+        {
+            
+            return new string[] { "value1", "value2" };
+        }
+
+        //// GET: api/Customer/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Customer
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Customer/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //    throw excep
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
